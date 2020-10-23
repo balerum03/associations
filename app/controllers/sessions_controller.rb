1b33abbfb5a0e947e003f22e53d79a9b14eec5cb
Bbosa <bbosa2009@gmail.com>
2020-10-23 00:30:32 -0700

    added user sessions controllers and logic

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,39 @@
+class SessionsController < ApplicationController
+
+  def new
+    
+  end 
+
+  def create
+   @user = User.find_by(username: params[:username])
+   if @user
+      sessions[:user_id] = @user.id
+      redirect_to '/welcome'
+   else
+      redirect_to '/login'
+   end
+  end
+
+  def login
+  end 
+
+  def welcome
+  end 
+#   def new; end
+#   def create
+#     @user = User.find_by(params[:username])
+#     if @user
+#       session[:user_id] = @user.id
+#       redirect_to root_path(@user.username, @user), notice: 'You are logged in'
+#     else
+#       flash.now[:alert] = 'User not found, please try again'
+#     end
+#   end
+#   def destroy
+#     session.delete(:user_id)
+#     @current_user = nil
+#     redirect_to root_path, notice: 'Logged Out'
+#   end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :events
-  resources :users
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'users#index', :path_prefix => '/:username'
-  resources :logins
-  delete 'logout' => 'logins#destroy'
-  get 'login' => 'logins#new'
+  # root 'welcome#index'# :path_prefix => '/:username'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  # delete 'logo, ut' => 'logins#destroy'
+  # get 'login' => 'logins#new'
 end
