491a316d7dd80f9fe0f707542bae1bc15148f0ff
balerum03 <adafer123.angra@gmail.com>
2020-10-28 18:14:51 -0600

    Made test file with rspec for sessions_controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,22 @@
+
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  let!(:user) { User.create(username: 'Balerum03', email: 'testing@test.com') }
+  let!(:test_user) { User.create(username: 'Balerum03', email: 'test@test.com') }
+  let!(:event) { Event.create(title: 'A very nice and well tough tittle.', body: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum', date: '2020-03-10', creator_id: test_user.id) }
 
-  it 'checks for the creation of a session with a valid user' do
-    post'/login', params: { username: { username: user.username } }
-    expect(subject).to redirect_to(new_sessions_path)
+  it 'checks for the creation of a session with a invalid user' do
+    post :create, params: {username: nil}
+    expect(subject).to redirect_to('/login')
   end
 
+  it 'checks for the creation of a session with a valid user' do
+    post :create, params: {username: test_user.username}
+    expect(subject).to redirect_to(root_path)
+  end
+
+  it 'Takes you to the Home page when the session is destroy' do
+    get :destroy
+    expect(subject).to redirect_to(root_path)
+  end
 end
